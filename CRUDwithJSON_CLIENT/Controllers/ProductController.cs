fb901bf0cd4498828c28e9d19e9ae4499ede83e1
oswaldoadidas <oswaldoadidas@hotmail.com>
2016-08-18 11:23:23 -0500

    we can already edit and delete data from a DB

--- CRUDwithJSON_CLIENT/Controllers/ProductController.cs
+++ CRUDwithJSON_CLIENT/Controllers/ProductController.cs
@@ -17,5 +17,43 @@ namespace CRUDwithJSON_CLIENT.Models
             ViewBag.listProducts = psc.findAll();
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {   
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductViewModel pvm)
+        {
+            ProductServiceClient psc = new ProductServiceClient();
+            psc.create(pvm.product);
+            return RedirectToAction("Index");
+        }
+
+       
+        public ActionResult Delete(string id)
+        {
+            ProductServiceClient psc = new ProductServiceClient();
+            psc.delete(psc.find(id));
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            ProductServiceClient psc = new ProductServiceClient();
+            ProductViewModel pvm = new ProductViewModel();
+            pvm.product = psc.find(id);
+            return View("Edit",pvm);
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductViewModel pvm)
+        {
+            ProductServiceClient psc = new ProductServiceClient();
+            psc.edit(pvm.product);
+            return RedirectToAction("Index");
+        }
     }
 }
